e22b611d2dc5b36f4ba6cc3ba3b2d88ea94bdf0c
Ben Valdebenito <benvb1234@gmail.com>
2022-03-03 20:17:33 -0800

    Working on index.php

---
+++ department.php
@@ -0,0 +1,149 @@
+<?php require_once('config.php'); ?>
+<!-- TCSS 445 : Winter 2021
+Author: Ben Valdebenito
+This file creates the menu option for project and output certan information about given options.
+-->
+<!-- Assignment 4 Template -->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Assignment 4 Demo</title>
+        <!-- add a reference to the external stylesheet -->
+        <link rel="stylesheet" href="https://bootswatch.com/4/solar/bootstrap.min.css">
+    </head>
+
+    <body>
+        <!-- START -- Add HTML code for the top menu section (navigation bar) -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">assign4</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarColor02">
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Home
+                                <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="employee.php">Employee</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="department.php">Department</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="project.php">Project</a>
+                        </li>
+<!--
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Separated link</a>
+                            </div>
+                        </li>
+-->
+                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-sm-2" type="text" placeholder="Search">
+                        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <!-- END -- Add HTML code for the top menu section (navigation bar) -->
+
+        <div class="jumbotron">
+            <p class="lead">Select a department's name</p>
+            <hr class="my-4">
+            <form method="GET" action="department.php">
+                <select name="dept" onchange='this.form.submit()'>
+                    <option selected>Select a name</option>
+
+                    <?php
+                    $connection = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
+                    if ( mysqli_connect_errno() )
+                    {
+                        die( mysqli_connect_error() );
+                    }
+                    // Select the department name to display as menu option
+                    $sql = "select Dname, Dnumber from DEPARTMENT";
+                    if ($result = mysqli_query($connection, $sql))
+                    {
+                        // loop through the data
+                        while($row = mysqli_fetch_assoc($result))
+                        {
+                            echo '<option value="' . $row['Dnumber'] . '">';
+                            echo $row['Dname'];
+                            echo "</option>";
+                        }
+                        // release the memory used by the result set
+                        mysqli_free_result($result);
+                    }
+                    ?>
+                </select>
+                <?php
+                if ($_SERVER["REQUEST_METHOD"] == "GET")
+                {
+                    if (isset($_GET['dept']) )
+                    {
+                ?>
+                <p>&nbsp;</p>
+                <table class="table table-hover">
+                    <thead>
+                        <tr class="table-success">
+                            <th scope="col">Last Name</th>
+                            <th scope="col">First Name</th>
+                            <th scope="col">Supervisor's Last Name</th>
+                            <th scope="col">Supervisor's First Name</th>
+                        </tr>
+                    </thead>
+                    <?php
+                        if ( mysqli_connect_errno() )
+                        {
+                            die( mysqli_connect_error() );
+                        }
+                        // Grab everything when our Dnumber matches and so does department Id. I struggled here
+                        // when i tried setting the GET command to the literal name of the department. I think the issue was
+                        // me not making it a string literal with quotes
+                        $sql = "  SELECT *
+                            FROM EMPLOYEE, DEPARTMENT
+                            WHERE Dnumber = {$_GET['dept']}  AND
+                                  EMPLOYEE.Dno = DEPARTMENT.Dnumber";
+
+                        if ($result = mysqli_query($connection, $sql))
+                        {
+                            while($row = mysqli_fetch_assoc($result))
+                            {
+                                $sql2 = "  SELECT * FROM EMPLOYEE WHERE EMPLOYEE.Ssn = {$row['Super_ssn']}";
+                                $result2 = mysqli_query($connection, $sql2);
+                                $row2 = mysqli_fetch_assoc($result2);
+
+                    ?>
+                    <tr>
+                        <td><?php echo $row['Lname'] ?></td>
+                        <td><?php echo $row['Fname'] ?></td>
+                        <td><?php echo $row2['Lname'] ?></td>
+                        <td><?php echo $row2['Fname'] ?></td>
+                    </tr>
+                    <?php
+                            }
+                            // release the memory used by the result set
+                            mysqli_free_result($result);
+                        }
+                    } // end if (isset)
+                } // end if ($_SERVER)
+                    ?>
+                </table>
+            </form>
+        </div>
+    </body>
+</html>
---
+++ project.php
@@ -0,0 +1,143 @@
+<?php require_once('config.php'); ?>
+<!-- TCSS 445 : Winter 2021
+Author: Ben Valdebenito
+This file creates the menu option for project and output certan information about given options.
+-->
+<!-- Assignment 4 Template -->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Assignment 4 Demo</title>
+        <!-- add a reference to the external stylesheet -->
+        <link rel="stylesheet" href="https://bootswatch.com/4/solar/bootstrap.min.css">
+    </head>
+
+    <body>
+        <!-- START -- Add HTML code for the top menu section (navigation bar) -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">assign4</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarColor02">
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Home
+                                <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="employee.php">Employee</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="department.php">Department</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="project.php">Project</a>
+                        </li>
+<!--
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Separated link</a>
+                            </div>
+                        </li>
+-->
+                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-sm-2" type="text" placeholder="Search">
+                        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <!-- END -- Add HTML code for the top menu section (navigation bar) -->
+
+        <div class="jumbotron">
+            <p class="lead">Select a project's location</p>
+            <hr class="my-4">
+            <form method="GET" action="project.php">
+                <select name="project" onchange='this.form.submit()'>
+                    <option selected>Select a location</option>
+
+                    <?php
+                    $connection = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
+                    if ( mysqli_connect_errno() )
+                    {
+                        die( mysqli_connect_error() );
+                    }
+                    $sql = "select * from DEPT_LOCATIONS";
+                    if ($result = mysqli_query($connection, $sql))
+                    {
+                        // loop through the data
+                        while($row = mysqli_fetch_assoc($result))
+                        {
+                            // I picked Dlocation since Dnum had duplcates
+                            echo '<option value="' . $row['Dlocation'] . '">';
+                            echo $row['Dlocation'];
+                            echo "</option>";
+                        }
+                        // release the memory used by the result set
+                        mysqli_free_result($result);
+                    }
+                    ?>
+                </select>
+                <?php
+                if ($_SERVER["REQUEST_METHOD"] == "GET")
+                {
+                    if (isset($_GET['project']) )
+                    {
+                ?>
+                <p>&nbsp;</p>
+                <table class="table table-hover">
+                    <thead>
+                        <tr class="table-success">
+                            <th scope="col">Project Name</th>
+                            <th scope="col">Controlling Department</th>
+                            <th scope="col">Manager's Last Name</th>
+                            <th scope="col">Managers Address</th>
+                            <th scope="col">Managers Birth Date</th>
+                        </tr>
+                    </thead>
+                    <?php
+                        if ( mysqli_connect_errno() )
+                        {
+                            die( mysqli_connect_error() );
+                        }
+                        // Unlike the other part, i decided to make one table by using join
+                        $sql = "SELECT * FROM PROJECT JOIN DEPARTMENT ON PROJECT.Dnum = DEPARTMENT.Dnumber JOIN EMPLOYEE ON EMPLOYEE.Ssn = DEPARTMENT.Mgr_ssn WHERE Plocation = '{$_GET['project']}'";
+                        if ($result = mysqli_query($connection, $sql))
+                        {
+                            while($row = mysqli_fetch_assoc($result))
+                            {
+
+
+                    ?>
+                    <tr>
+                        <td><?php echo $row['Pname'] ?></td>
+                        <td><?php echo $row['Dname'] ?></td>
+                        <td><?php echo $row['Lname'] ?></td>
+                        <td><?php echo $row['Address'] ?></td>
+                        <td><?php echo $row['Bdate'] ?></td>
+                    </tr>
+                    <?php
+                            }
+                            // release the memory used by the result set
+                            mysqli_free_result($result);
+                        }
+                    } // end if (isset)
+                } // end if ($_SERVER)
+                    ?>
+                </table>
+            </form>
+        </div>
+    </body>
+</html>
